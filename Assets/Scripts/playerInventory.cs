bf1dc3e97413c1477cca700696c2a8af9060db1a
Myra2036 <153661426+Myra2036@users.noreply.github.com>
2024-02-20 22:17:23 -0600

    made player inventory script

---
+++ Assets/Scripts/playerInventory.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerInventory : MonoBehaviour
+{
+    // Access player controller script
+    public PlayerController playerController;
+
+    // Make these vars accessible to outside scripts
+
+
+
+
+
+
+
+
+
+
+    public int flaskCount;
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
